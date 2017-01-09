e4851f2f01dba9129b4eca290c4a922b344c28eb
hcho5 <hcho5@wellesley.edu>
2017-01-09 06:31:06 -0800

    Add files via upload

---
+++ showhide.js
@@ -0,0 +1,39 @@
+$(document).keypress(function(e){
+    var key = e.which;
+    if (key == 116) {
+        // if the user pressed 't' (for 'toggle'):
+        $('#comment').show();
+    }
+    setTimeout(fade_out, 4000);
+});
+
+function fade_out() {
+  $("#comment").fadeOut().hide();
+  setTimeout(show_options,500);
+}
+
+function show_options() {
+  $("#wrapper").show();
+  $("#response1").show();
+  $("#response2").show();
+}
+
+function toggle_visibility(id) {
+   var e = document.getElementById(id);
+   if(e.style.display === 'none')
+      e.style.display = 'block';
+   else
+      e.style.display = 'none';
+}
+
+// $('#show').click(function() {
+// 	$('#show').css('display','none');
+// 	$('#data').show();
+// 	$('#hide').show();
+// });
+
+// $('#hide').click(function() {
+// 	$('#hide').css('display','none');
+// 	$('#data').hide();
+// 	$('#show').show();
+// });
